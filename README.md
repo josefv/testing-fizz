e14e8519ba032f5a343418085c4b06a15ea51d1f
josefv <josefv@users.noreply.github.com>
2014-10-21 11:02:34 -0400

    cleaned up files

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
-Run the following command:
+Run the following commands:
 
-
-`npm install -g mocha chai`
-
+npm install -g mocha chai
+npm install chai
 
 Then to run your tests run:
 
+mocha -w fizzbuzz.js
 
-`mocha -w fizzbuzz.js`
-
-
-This will start a test watch server that
-will rerun your tests everytime you save the file.
+This will start a test watch server that will rerun your tests everytime you save the file.
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,11 +3,12 @@ var expect = require('chai').expect
 /* Fizzbuzz assignment
 
 - fizzbuzz should return fizz for numbers that are divisible by 3
-- fizzbuzz should return buzz for numbers taht are divisible by 5
-- fizzbuzz should return fizzbuzz for numbers taht are divisible by 3 and 5
-- fizzbuzz should return the number given if it does not meet one of the above  rules
+- fizzbuzz should return buzz for numbers that are divisible by 5
+- fizzbuzz should return fizzbuzz for numbers that are divisible by 3 and 5
+- fizzbuzz should return the number given if it does not meet one of the above rules
 
 */
+
 function fizzbuzz (number) {
   if (number % 15 === 0) {
     return 'fizzbuzz';
@@ -19,19 +20,19 @@ function fizzbuzz (number) {
     return 'buzz';
   }
   return number;
-}
-// Write your code here inside fizzbuzz()
+};
 
+// Write your code here ^^^^^^^ inside fizzbuzz()
+// Write tests below to drive out the behavior of fizzbuzz
+// There must be at least 5 tests in total when you are done
 
-// Write tests here to drive out the behavior of fizzbuzz
-// There must be atleast 5 tests in total when you are done
 /*
     Remember - Red/Green/Refactor
     - You are not allowed to write any code unless it is to make a failing test pass.
     - You are not allowed to write any more of a test than is sufficient to fail.
     - You are not allowed to write any more production code than is sufficient to pass the one failing test.
-
 */
+
 describe('Fizzbuzz', function () {
 
   it('should exist', function () {
@@ -55,21 +56,3 @@ describe('Fizzbuzz', function () {
   });
 
 });
-
-/*
-it('should return 2 if given 2', function () {
-      expect(fizzbuzz(2)).to.equal(2);
-  });
-
-  it('should return fizz if given 3', function () {
-      expect(fizzbuzz(3)).to.equal('fizz');
-  });
-
-  it('should return buzz if given 5', function () {
-      expect(fizzbuzz(5)).to.equal('buzz');
-  });
-
-  it('should return fizzbuzz if given 15', function () {
-      expect(fizzbuzz(15)).to.equal('fizzbuzz');
-  });
-*/
